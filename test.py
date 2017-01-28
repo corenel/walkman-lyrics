3ad1c55796a8fafa3cbbc7e922bd4f570c6e6ff1
corenel <xxdsox@gmail.com>
2017-01-28 12:43:38 +0800

    init

---
+++ test.py
@@ -0,0 +1,45 @@
+import argparse
+from utils import *
+
+
+def qprint(*args, **kwargs):
+    global argsp
+    if not argsp.quiet:
+        print(*args, **kwargs)
+
+
+if __name__ == '__main__':
+    # arguments parser
+    args = argparse.ArgumentParser()
+    args.add_argument('path', help='Root path for Walkman')
+    args.add_argument('-m', '--mode',
+                      help='Mode of LRC file',
+                      choices=['trans', 'original', 'both'],
+                      default='both')
+    args.add_argument('-f', '--format',
+                      help='Format to combine both types of lyrics\n'
+                           'Default: \'{orig} / {trans}\'',
+                      default='{orig} / {trans}')
+    args.add_argument('-q', '--quiet',
+                      help='Quiet mode, no prompt output to STDOUT, choose first search result by default.',
+                      action='store_true')
+    args.add_argument('-d', '--default',
+                      help='Choose first search result by default.',
+                      action='store_true')
+    argsp = args.parse_args()
+    argsp.default |= argsp.quiet
+
+    # list all music files
+    fileList = getFileList(argsp.path)
+
+    # get lyrics
+    for song in fileList:
+        qprint(song)
+        # lyric = getLyrics(songTitle=song['title'],
+        #                          songDefault=argsp.default,
+        #                          lyricMode=argsp.mode,
+        #                          verbose=not argsp.quiet)
+        # writeLyrics(filePath=song['path'],
+        #             fileName=song['name'],
+        #             lyrics="aaaaa")
+    qprint('Done!')
---
+++ utils.py
@@ -0,0 +1,124 @@
+import requests
+import re
+import json
+from os import listdir, walk, path
+from tinytag import TinyTag
+from collections import defaultdict
+
+
+def getFileList(fileDir):
+    fileFormat = ['.m4a', '.mp3']
+    fileList = []
+    if path.exists(fileDir):
+        for root, subFolders, fileNames in walk(fileDir):
+            fileNames = [x for x in fileNames
+                         if path.splitext(x)[1] in fileFormat]
+            if (fileNames):
+                for fileName in fileNames:
+                    tags = TinyTag.get(path.join(root, fileName))
+                    fileList.append({
+                        'path': root,
+                        'name': fileName,
+                        'title': tags.title or path.splitext(fileName)[0],
+                        'artist': tags.artist or ''
+                    })
+        return fileList
+    else:
+        print('Error: target directory not exists')
+        exit()
+
+
+def getLyrics(songTitle='', songDefault=False, lyricMode='both', verbose=False):
+    # requests settings
+    session = requests.Session()
+    sn.headers.update({"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36",
+                       "Referer": "http://music.163.com/search/"})
+    search = 'http://music.163.com/api/search/get/web'
+    # get song id
+    qprint('Searching %s' % songTitle)
+    res = sn.post(search, data={
+        "s": songTitle,
+        "type": 1,
+        "offset": 0,
+        "limit": 10
+    }).json()
+    songs = res['result']['songs']
+    if len(songs) == 0:
+        qprint("No result found for \"%s\"." % songTitle)
+        return ''
+    elif len(songs) == 1 or songDefault:
+        song = songs[0]['id']
+        sid = 0
+    else:
+        qprint("Choose a song to download, or ^C to exit.\n")
+        for i, s in enumerate(songs):
+            qprint("%-5s\x1b[1m%s\x1b[0m \x1b[2m(%d)\x1b[0m" % ("%s." % i, s['name'], s['id']))
+            qprint("     \x1b[0m%s | \x1b[3m%s\x1b[0m" %
+                   ("; ".join([j['name'] for j in s['artists']]), s['album']['name']))
+            qprint()
+        sid = input("Song ID: ")
+        while not sid.isdecimal() or int(sid) < 0 or int(sid) >= len(songs):
+            qprint("%s is invalid. Please enter a integer between 0 and %s." % (sid, len(songs)))
+            sid = input("Song ID: ")
+        sid = int(sid)
+        song = songs[int(sid)]['id']
+    # get song info
+    songinfo = [songs[sid]['name'], "; ".join([j['name'] for j in songs[sid]['artists']]), songs[sid]['album']['name']]
+    songstr = "     \x1b[1m%s\x1b[0m \x1b[2m(%d)\x1b[0m\n" % (songinfo[0], songs[sid]['id'])
+    songstr += "     \x1b[0m%s | \x1b[3m%s\x1b[0m\n" % (songinfo[1], songinfo[2])
+    qprint()
+    qprint("Trying to download song %s:" % song)
+    qprint(songstr)
+    req = sn.get("http://music.163.com/api/song/lyric?lv=1&kv=1&tv=-1&id=%s" % song).json()
+    # get lyrics
+    if req.get('lrc', None) is None or req['lrc'].get('lyric', None) is None:
+        qprint("No lyrics found.")
+        exit()
+    # lyric mode
+    has_trans = True
+    if lyricMode in ['trans', 'both']:
+        if req.get('tlyric', None) is None or req['tlyric'].get('lyric', None) is None:
+            qprint("No translation found, fallback to original lyrics.")
+            has_trans = False
+            lyricMode = 'original'
+    else:
+        has_trans = False
+    # reformat lyrics
+    org = defaultdict(str)
+    trans = defaultdict(str)
+    r = re.compile(r"\[(?P<tag>[0-9:.\]\[]+)\](?P<lrc>.*)")
+    for i in req['lrc']['lyric'].split('\n'):
+        rm = r.match(i)
+        if not rm:
+            continue
+        ln = rm.groupdict()
+        for j in ln['tag'].split(']['):
+            org[j] = ln['lrc'].strip()
+    if has_trans:
+        for i in req['tlyric']['lyric'].split('\n'):
+            rm = r.match(i)
+            if not rm:
+                continue
+            ln = rm.groupdict()
+            for j in ln['tag'].split(']['):
+                trans[j] = ln['lrc'].strip()
+    out = []
+    for i in sorted(org):
+        if ap.mode == 'original' or not trans[i].strip() or trans[i].strip() == org[i].strip():
+            line = ["[{tag}]{orig}"]
+        elif ap.mode == 'trans' or not org[i].strip():
+            line = ['[{tag}]{trans}']
+        elif ap.mode == 'both':
+            line = ['[{tag}]' + i for i in ap.format.split('\n')]
+        for l in line:
+            out.append(l.format(tag=i, orig=org[i], trans=trans[i]))
+    return out
+
+
+def writeLyrics(filePath, fileName, lyrics):
+    lyricsFile = path.join(filePath, path.splitext(fileName)[0]) + '.lrc'
+    if path.exists(lyricsFile):
+        print('Lyrics already exists for %s' % lyricsFile)
+    else:
+        with open(lyricsFile, 'w') as of:
+            of.write('\n'.join(lyrics))
